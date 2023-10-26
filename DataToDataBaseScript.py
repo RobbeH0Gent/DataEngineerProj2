443683fd86cf97dd787003d24007aa1ac68a61db
Flor-DeBruyne <flor.debruyne@student.hogent.be>
2023-10-26 15:21:51 +0200

    oltp null values

--- DataToDataBaseScript.py
+++ DataToDataBaseScript.py
@@ -1,43 +1,45 @@
 from dotenv import load_dotenv   #for python-dotenv method
 import os
 import pandas as pd
-from sqlalchemy import create_engine, Table, MetaData
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine, MetaData
 
 load_dotenv()
-
+  
 csv_dir = 'Data/' #path waar de data csv files staan
 SERVER = os.environ.get('SERVER')
 DATABASE = os.environ.get('DATABASE')
 UID = os.environ.get('USER')
 PWD = os.environ.get('PASSWORD')
 
-# NOT EVERY CSV IS IN THIS DICT
+
 
 table_file = {
     # 'Account' : 'Account.csv', # DONE
+    # # 'Persoon' : 'Persoon.csv', # ALOT OF COLUMNS NOT IN OLTP - TEAM
+    # 'Contactfiche' : 'Contact.csv', # DUPLICATES
     # 'Activiteitscode' : 'Activiteitscode.csv', # DONE
     # 'Account_ActiviteitsCode' : 'Account_activiteitscode.csv',  # DONE  
     # 'Account_Financiele_Data' : 'Account_financiele_data.csv',     # DUPLICATES  
     # 'Activiteit_Vereist_Contact' : 'Activiteit_vereist_contact.csv', # DUPLICATES
-    # 'Afpsraak_Alle' : 'Afspraak_alle.csv', # DONE
+    # 'Afspraak_Alle' : 'Afspraak_alle.csv', # DONE
     # 'Afspraak_Account_Gelinkt' : 'Afspraak_account_gelinkt_cleaned.csv', # FK PROBLEM
-    # 'Afspraak_Betreft_Account' : 'Afspraak betreft account_cleaned.csv', # FK PROBLEM
     # 'Afspraak_Betreft_Contactfiche': 'Afspraak betreft contact_cleaned.csv', # FK PROBLEM
+    # 'Afspraak_Betreft_Account' : 'Afspraak betreft account_cleaned.csv', # FK PROBLEM
     # 'Afspraak_Vereist_Contact' : 'Activiteit_vereist_contact.csv',   # FK PROBLEM  
     # 'Campagne' : 'Campagne.csv', # DONE
     # 'CDI_Mailing' : 'CDI_mailing.csv', # DONE
     # 'CDI_PageView' : 'cdi_pageviews.csv', # PROBLEM WITH ';' 
-    # 'CDI_Sent_Email_Clicks' : 'CDI_sent_email_clicks.csv',  # DUPLICATES
-    # 'CDI_Visits' : 'CDI_visits.csv', # FK PROBLEM
     # 'CDI_Web_Content' : 'CDI_web_content.csv',  # DONE
-    # 'Contactfiche' : 'Contact.csv', # DUPLICATES
+    #    'CDI_Visits' : 'CDI_visits.csv', # FK PROBLEM
+    # 'CDI_Sent_Email_Clicks' : 'CDI_sent_email_clicks.csv',  # DUPLICATES
+    # 'Functie' : 'Functie.csv', # DUPLICATES
     # 'ContactFunctie' : 'Contact_functie.csv',  # FK PROBLEM
     # 'Gebruiker' : 'Gebruikers.csv', # DONE
     # 'Info_en_Klachten' : 'Info_en_klachten.csv', # FK PROBLEM
+    # 'Inschrijving' : 'Inschrijving.csv', # DUPLICATES
     # 'Lidmaatschap' : 'Lidmaatschap.csv', # FK PROBLEM
-    # 'Persoon' : 'Persoon.csv', # ALOT OF COLUMNS NOT IN OLTP - TEAM
-    # 'Sessie' : 'Sessie.csv' # FK PROBLEM
+    # 'Sessie' : 'Sessie.csv', # FK PROBLEM
+    # 'Sessie_Inschrijving': 'Sessie_inschrijving.csv'
 }
 
 
@@ -55,8 +57,14 @@ try:
 
         df = pd.read_csv(file_path, on_bad_lines='skip')
         df = df.rename(columns={csv_col: sql_col for csv_col, sql_col in zip(df.columns, table_columns)})
-        df.to_sql(table_name, con=engine, schema='dbo', if_exists='append', index=False)
 
+
+        if df.isnull().values.any():
+            df.dropna(inplace=True)
+
+
+        df.to_sql(table_name, con=engine, schema='dbo', if_exists='append', index=False)
+        print(f"{table_name}")
     print("Done")
 
 except Exception as e:
